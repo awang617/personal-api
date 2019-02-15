365a018f67f4651cd85e0b5e2f7652633510686f
awang617 <wanga18@mail.wlu.edu>
2019-02-14 16:20:52 -0800

    url parser so no warnings

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Project = require("./project.js")
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useMongoClient: true});
+const Project = require("./projects.js")
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useNewUrlParser: true});
 
 exports.Project = Project;
