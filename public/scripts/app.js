d027dd895307cc5653e99cf13d6d5dceb828bb4a
awang617 <wanga18@mail.wlu.edu>
2019-02-14 20:38:36 -0800

    adding front end js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,101 @@
 console.log("Sanity Check: JS is working!");
+var $projectList;
+var allProjects = [];
 
 $(document).ready(function(){
 
-// your code
+  $booksList = $('#projectTarget');
+  $.ajax({
+    method: 'GET',
+    url: '/api/projects',
+    success: handleSuccess,
+    error: handleError
+  });
+
+  $('#newBookForm').on('submit', function(e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/api/books',
+      data: $(this).serialize(),
+      success: newBookSuccess,
+      error: newBookError
+    });
+  });
+
+  $booksList.on('click', '.deleteBtn', function() {
+    console.log('clicked delete button to', '/api/books/'+$(this).attr('data-id'));
+    $.ajax({
+      method: 'DELETE',
+      url: '/api/books/'+$(this).attr('data-id'),
+      success: deleteBookSuccess,
+      error: deleteBookError
+    });
+  });
 
 });
+
+function getBookHtml(book) {
+  return `<hr>
+          <p>
+            <b>${book.title}</b>
+            by ${book.author.name}
+            <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${book._id}>Delete</button>
+          </p>`;
+}
+
+function getAllBooksHtml(books) {
+  return books.map(getBookHtml).join("");
+}
+
+// helper function to render all posts to view
+// note: we empty and re-render the collection each time our post data changes
+function render () {
+  // empty existing posts from view
+  $booksList.empty();
+
+  // pass `allBooks` into the template function
+  var booksHtml = getAllBooksHtml(allBooks);
+
+  // append html to the view
+  $booksList.append(booksHtml);
+};
+
+function handleSuccess(json) {
+  allProjects = json;
+  render();
+}
+
+function handleError(e) {
+  console.log('uh oh');
+  $('#bookTarget').text('Failed to load books, is the server working?');
+}
+
+function newBookSuccess(json) {
+  $('#newBookForm input').val('');
+  allBooks.push(json);
+  render();
+}
+
+function newBookError() {
+  console.log('newbook error!');
+}
+
+function deleteBookSuccess(json) {
+  var book = json;
+  console.log(json);
+  var bookId = book._id;
+  console.log('delete book', bookId);
+  // find the book with the correct ID and remove it from our allBooks array
+  for(var index = 0; index < allBooks.length; index++) {
+    if(allBooks[index]._id === bookId) {
+      allBooks.splice(index, 1);
+      break;  // we found our book - no reason to keep searching (this is why we didn't use forEach)
+    }
+  }
+  render();
+}
+
+function deleteBookError() {
+  console.log('deletebook error!');
+}
