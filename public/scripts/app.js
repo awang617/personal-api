80565180a60b497528370d1842e9348f73835e15
awang617 <wanga18@mail.wlu.edu>
2019-02-14 20:53:53 -0800

    renamed all instances of book to project

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ var allProjects = [];
 
 $(document).ready(function(){
 
-  $booksList = $('#projectTarget');
+  $projectList = $('#projectTarget');
   $.ajax({
     method: 'GET',
     url: '/api/projects',
@@ -12,53 +12,53 @@ $(document).ready(function(){
     error: handleError
   });
 
-  $('#newBookForm').on('submit', function(e) {
+  $('#newProjectForm').on('submit', function(e) {
     e.preventDefault();
     $.ajax({
       method: 'POST',
-      url: '/api/books',
+      url: '/api/projects',
       data: $(this).serialize(),
-      success: newBookSuccess,
-      error: newBookError
+      success: newProjectSuccess,
+      error: newProjectError
     });
   });
 
-  $booksList.on('click', '.deleteBtn', function() {
-    console.log('clicked delete button to', '/api/books/'+$(this).attr('data-id'));
+  $projectList.on('click', '.deleteBtn', function() {
+    console.log('clicked delete button to', '/api/projects/'+$(this).attr('data-id'));
     $.ajax({
       method: 'DELETE',
-      url: '/api/books/'+$(this).attr('data-id'),
-      success: deleteBookSuccess,
-      error: deleteBookError
+      url: '/api/projects/'+$(this).attr('data-id'),
+      success: deleteProjectSuccess,
+      error: deleteProjectError
     });
   });
 
 });
 
-function getBookHtml(book) {
+function getProjectHtml(project) {
   return `<hr>
           <p>
-            <b>${book.title}</b>
-            by ${book.author.name}
-            <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${book._id}>Delete</button>
+            <b>${project.name}</b>
+            , ${project.description} on ${project.date} find at ${project.githubUrl}
+            <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${project._id}>Delete</button>
           </p>`;
 }
 
-function getAllBooksHtml(books) {
-  return books.map(getBookHtml).join("");
+function getAllProjectsHtml(project) {
+  return project.map(getProjectHtml).join("");
 }
 
 // helper function to render all posts to view
 // note: we empty and re-render the collection each time our post data changes
 function render () {
   // empty existing posts from view
-  $booksList.empty();
+  $projectList.empty();
 
-  // pass `allBooks` into the template function
-  var booksHtml = getAllBooksHtml(allBooks);
+  // pass `allProject` into the template function
+  var projectHtml = getAllProjectsHtml(allProjects);
 
   // append html to the view
-  $booksList.append(booksHtml);
+  $projectList.append(projectHtml);
 };
 
 function handleSuccess(json) {
@@ -68,34 +68,34 @@ function handleSuccess(json) {
 
 function handleError(e) {
   console.log('uh oh');
-  $('#bookTarget').text('Failed to load books, is the server working?');
+  $('#projectTarget').text('Failed to load projects, is the server working?');
 }
 
-function newBookSuccess(json) {
-  $('#newBookForm input').val('');
-  allBooks.push(json);
+function newProjectSuccess(json) {
+  $('#newProjectForm input').val('');
+  allProjects.push(json);
   render();
 }
 
-function newBookError() {
-  console.log('newbook error!');
+function newProjectError() {
+  console.log('new project error!');
 }
 
-function deleteBookSuccess(json) {
-  var book = json;
+function deleteProjectSuccess(json) {
+  var project = json;
   console.log(json);
-  var bookId = book._id;
-  console.log('delete book', bookId);
-  // find the book with the correct ID and remove it from our allBooks array
-  for(var index = 0; index < allBooks.length; index++) {
-    if(allBooks[index]._id === bookId) {
-      allBooks.splice(index, 1);
-      break;  // we found our book - no reason to keep searching (this is why we didn't use forEach)
+  var projectId = project._id;
+  console.log('delete project', projectId);
+  // find the project with the correct ID and remove it from our allProjects array
+  for(var index = 0; index < allProjects.length; index++) {
+    if(allProjects[index]._id === projectId) {
+      allProjects.splice(index, 1);
+      break;  // we found our project - no reason to keep searching (this is why we didn't use forEach)
     }
   }
   render();
 }
 
-function deleteBookError() {
-  console.log('deletebook error!');
+function deleteProjectError() {
+  console.log('delete project error!');
 }
